fcade529606b994c0e8072a3a396adea2b6c3bb7
Indro De <indro.de@gmail.com>
2015-02-08 15:28:21 +0100

    Adjust puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,10 @@
-root = "#{Dir.getwd}"
+root = "/home/akira/zenpunch"
 daemonize true
-bind "unix://#{root}/../../tmp/puma/socket"
-pidfile "#{root}/../../tmp/puma/pid"
-state_path "#{root}/../../tmp/puma/state"
-rackup "#{root}/config.ru"
+bind "unix://#{root}/tmp/socket"
+pidfile "#{root}/tmp/pid"
+state_path "#{root}/tmp/state"
+rackup "#{root}/current/config.ru"
 
-threads 4, 8
+threads 2, 4
 
 activate_control_app
-
