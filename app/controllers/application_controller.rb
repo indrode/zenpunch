65dd55db8a5e6548e32f05905561635f638ff569
Indro De <indro.de@gmail.com>
2015-03-15 13:37:25 +0100

    Bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def routing_error
-    redirect_to controller: :static, action: :index, :status: 404
+    redirect_to controller: :static, action: :index, status: 404
   end
 end
