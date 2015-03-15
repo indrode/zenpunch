26609bf86163da31025997e422ac6c40d0cc26e3
Indro De <indro.de@gmail.com>
2015-03-15 12:49:21 +0100

    Redirect 404s to static#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def routing_error
+    redirect_to controller: :static, action: :index, :status: 404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static#index'
   resources :plex, only: %i(index update)
+  match '*path', to: 'application#routing_error', via: :all
 end
