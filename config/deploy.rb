0ed31040e7c35caf8b42aab8fd1a8f049389bb08
Indro De <indro.de@gmail.com>
2015-06-06 14:41:57 +0200

    Fix mina deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,20 @@
-require 'mina/chruby'
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/puma'
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rbenv'
 
 set :domain, 'tetsuo'
 set :deploy_to, '/home/akira/zenpunch'
 set :repository, 'git://github.com/indrode/zenpunch-api.git'
 set :branch, 'master'
-set :chruby_path, "/usr/local/share/chruby/chruby.sh"
-set :bundle_bin, '/home/akira/.gem/ruby/2.0.0/bin/bundle'
-
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['log']
+set :shared_paths, ['log', 'tmp/sockets', 'tmp/pids']
 
-# Optional settings:
-set :user, 'akira'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+task :environment do
+  invoke :'rbenv:load'
+end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
 # For Rails apps, we'll make some of the shared paths that are shared between
@@ -48,15 +35,15 @@ task :deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
-    invoke :'chruby[ruby-2.0.0-p247]'
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      # This kills the current Puma process, then restarts it
+      queue "kill $(cat #{deploy_to}/#{shared_path}/tmp/pids/puma.pid)"
+      queue "cd #{deploy_to}/#{current_path} && bundle exec puma -C config/puma.rb"
     end
   end
 end
