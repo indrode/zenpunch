4d3e09aa26f5be1080ca5f03ce5999719d133d81
Indro De <indro.de@gmail.com>
2015-07-07 23:08:36 +0200

    Add anti-spam logic

--- app/controllers/deca_controller.rb
+++ app/controllers/deca_controller.rb
@@ -1,12 +1,14 @@
 class DecaController < ApplicationController
   def create
-    DecaMailer.contact_email(contact_params).deliver_now
-    redirect_to ENV['DECA_REDIRECT'] || root_path
+    if contact_params[:alternative].empty?
+      DecaMailer.contact_email(contact_params).deliver_now
+      redirect_to ENV['DECA_REDIRECT'] || root_path
+    end
   end
 
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :email, :phone, :msg)
+    params.require(:contact).permit(:name, :email, :phone, :msg, :alternative)
   end
 end
