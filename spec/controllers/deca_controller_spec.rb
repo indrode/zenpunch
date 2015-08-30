f50411f0f2fa0a2161c68d69e10f22146088b40a
Indro De <indro.de@gmail.com>
2015-08-30 14:15:12 +0200

    Fix specs

--- spec/controllers/deca_controller_spec.rb
+++ spec/controllers/deca_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DecaController, type: :controller do
   describe 'POST #create' do
     it 'redirects after sending email' do
-      post :create, contact: { name: 'test' }
+      post :create, contact: { name: 'test', phone: '555-555-5555' }
       expect(response).to be_redirect
     end
   end
