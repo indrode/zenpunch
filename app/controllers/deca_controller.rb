8f6739bb72cc5aa117db3a67b6dc87867c92b166
Indro De <indro.de@gmail.com>
2015-11-24 14:26:02 +0100

    Refactor Deca controller

--- app/controllers/deca_controller.rb
+++ app/controllers/deca_controller.rb
@@ -1,13 +1,16 @@
 class DecaController < ApplicationController
+  before_filter :validate_params!, only: [:create]
+
   def create
-    # TODO: refactor
-    if contact_params[:alternative].nil? && contact_params[:phone].strip != '123456'
-      DecaMailer.contact_email(contact_params).deliver_now
-    end
+    DecaMailer.contact_email(contact_params).deliver_now
     redirect_to ENV['DECA_REDIRECT'] || root_path
   end
 
   private
+
+  def validate_params!
+    render text: 'Invalid data!' unless (contact_params[:alternative].nil? && contact_params[:phone].strip != '123456')
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :email, :phone, :msg, :alternative)
--- spec/controllers/deca_controller_spec.rb
+++ spec/controllers/deca_controller_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe DecaController, type: :controller do
       post :create, contact: { name: 'test', phone: '555-555-5555' }
       expect(response).to be_redirect
     end
+
+    it 'fails if validation failed' do
+      post :create, contact: { name: 'test', phone: '123456' }
+      expect(response.body).to eq('Invalid data!')
+    end
+
+    it 'fails if spam check activated' do
+      post :create, contact: { name: 'test', alternative: 'my@mail.com' }
+      expect(response.body).to eq('Invalid data!')
+    end
   end
 end
